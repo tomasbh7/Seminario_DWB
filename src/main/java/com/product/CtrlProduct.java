b68102702a4c12f6a7d30d8fd740c4ad25b864d9
tomas <tomas.b.h@ciencias.unam.mx>
2026-02-26 13:57:17 -0600

    Primera implementación de la práctica2

--- src/main/java/com/product/CtrlProduct.java
+++ src/main/java/com/product/CtrlProduct.java
@@ -1,5 +1,28 @@
 package com.product;
 
-public class CtrlProduct {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
+@RestController
+public class CtrlProduct {
+private List<Category> categories;
+    
+    public CtrlProduct() {
+        categories = new ArrayList<>();
+        categories.add(new Category(1, "Lentes", "Lts", 1));
+        categories.add(new Category(2, "Relojes", "Rljs", 1));
+    }
+    
+    @GetMapping("/category")
+    public List<Category> getCategories() {
+        List<Category> activeCategories = new ArrayList<>();
+        for (Category c : categories) {
+            if (c.getStatus() == 1) {
+                activeCategories.add(c);
+            }
+        }
+        return activeCategories;
+    }
 }
