c1aff87df9592cf44d6d1d3cfbc55c67106dda81
Ivan Gonzalez <ivan_gonzalez@ciencias.unam.mx>
2026-03-13 22:44:41 -0600

    punto 4 practica 4

--- src/main/java/com/product/api/service/SvcCategoryImplementacion.java
+++ src/main/java/com/product/api/service/SvcCategoryImplementacion.java
@@ -1,11 +1,15 @@
 package com.product.api.service;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.dao.DataAccessException;
 
 import com.product.api.entity.Category;
 import com.product.api.repository.RepoCategory;
+import com.product.exception.ApiException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
@@ -18,8 +22,19 @@ public class SvcCategoryImplementacion implements SvcCategory {
     @Override
     public ResponseEntity<List<Category>> getCategories() {
 
-        List<Category> categories = repoCategory.getCategories();
+        try {
 
-        return new ResponseEntity<>(categories, HttpStatus.OK);
+            List<Category> categories = repoCategory.getCategories();
+            return new ResponseEntity<>(categories, HttpStatus.OK);
+
+        } catch (DataAccessException e) {
+
+            throw new ApiException(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Error al consultar las categorías en la base de datos"
+            );
+
+        }
+
     }
 }
