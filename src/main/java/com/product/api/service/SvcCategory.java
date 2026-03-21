48b8b6b884a59bf2c59a28e60b3085ed66eb891c
Ivan Gonzalez <ivan_gonzalez@ciencias.unam.mx>
2026-03-20 23:55:10 -0600

    puntos 3 y 4

--- src/main/java/com/product/api/service/SvcCategory.java
+++ src/main/java/com/product/api/service/SvcCategory.java
@@ -1,8 +1,21 @@
 package com.product.api.service;
-import org.springframework.http.ResponseEntity;
+
 import java.util.List;
 import com.product.api.entity.Category;
+import com.product.api.dto.DtoCategoryIn;
 
 public interface SvcCategory {
-	public ResponseEntity<List<Category>> getCategories();
-	}
+
+    // Reemplaza getCategories()
+    public List<Category> findAll();
+
+    public List<Category> findActive();
+
+    public void create(DtoCategoryIn in);
+
+    public void update(DtoCategoryIn in, Integer id);
+
+    public void enable(Integer id);
+
+    public void disable(Integer id);
+}
